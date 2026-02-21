50541554e9d9762c8dd5a619c25f8cb0c7ef901f
Muammad Ameen05 <muhammadameenbirahmani6@gmail.com>
2026-02-21 15:28:31 +0500

    third test

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,7 @@
-// src/App.test.js
 import { render, screen } from '@testing-library/react';
-import { act } from 'react';
 import App from './App';
 
 test('renders app', () => {
-  act(() => {
-    render(<App />);
-  });
+  render(<App />);
   expect(screen.getByText(/learn react/i)).toBeInTheDocument();
 });
