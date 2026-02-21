2439f46e2e09a8fb1b32aa687c5831ef31c3bfbd
Muammad Ameen05 <muhammadameenbirahmani6@gmail.com>
2026-02-21 15:18:31 +0500

    second test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 // src/App.test.js
 import { render, screen } from '@testing-library/react';
+import { act } from 'react';
 import App from './App';
 
 test('renders app', () => {
-  render(<App />);
+  act(() => {
+    render(<App />);
+  });
   expect(screen.getByText(/learn react/i)).toBeInTheDocument();
 });
