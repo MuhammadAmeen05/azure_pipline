9f754532a13d3aea960fee1a6e3f3b110c606c28
Muammad Ameen05 <muhammadameenbirahmani6@gmail.com>
2026-02-21 15:50:35 +0500

    fifth test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,13 @@
+// src/App.test.js
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 test('renders app', () => {
-  render(<App />);
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
   expect(screen.getByText(/learn react/i)).toBeInTheDocument();
 });
